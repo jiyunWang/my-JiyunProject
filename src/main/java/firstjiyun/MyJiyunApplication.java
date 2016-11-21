9c0c25dbe8660860d5c09a94c0cb120f8993ff63
jiyun <jiyoon3000@gmail.com>
2016-11-21 21:32:09 +0900

    firstCommit

---
+++ src/main/java/firstjiyun/MyJiyunApplication.java
@@ -0,0 +1,12 @@
+package firstjiyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJiyunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJiyunApplication.class, args);
+	}
+}
